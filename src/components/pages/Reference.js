9f0aae54606180aee8b8e9fb861c66833957c3e1
boseungdl <96tmdqh@naver.com>
2022-04-21 18:19:18 +0900

    4/2

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -9,78 +9,152 @@ import ReferCont from "../includes/ReferCont";
 import Loading from "../basics/Loading";
 import { gsap } from "gsap";
 import axios from "axios";
-// function Reference(){
-//     return (
-//         <>
-//             <Header color="light" />
-//             <Contents>
-//                 <Title title={["reference","book"]} color="light" />
-//                 <ReferCont color="light" />
-//                 <Contact />
-//             </Contents>
-//             <Footer color="light" />
-//         </>
-//     )
+
+// class Reference extends React.Component {
+//     state = {
+//         isLoading: true,
+//         refers: [],
+//     }
+//     mainAnimation = () => {
+//         setTimeout(() => {
+//             gsap.to("#header", {
+//                 duration: 0.8,
+//                 top: 0,
+//             });
+//             gsap.to("#footer", {
+//                 duration: 0.8,
+//                 bottom: 0,
+//                 delay: 0.2,
+//             });
+//             gsap.to(".cont__title strong", {
+//                 duration: 0.7,
+//                 y: 0,
+//                 opacity: 1,
+//                 delay: 1.0,
+//                 ease: "power4.out",
+//                 scale : 1,
+//             });
+//             gsap.to(".cont__title em", {
+//                 duration: 0.7,
+//                 y: 0,
+//                 opacity: 0.9,
+//                 delay: 1.3,
+//                 ease: "power4.out",
+//                 scale : 1,
+//             });
+//             gsap.to(".refer__inner", {
+//                 duration: 0.7,
+//                 y: 0,
+//                 opacity: 1,
+//                 delay: 1.5,
+//                 ease: "power4.out"
+//             });
+//         }, 1000)
+//     }
+//     getSite = async () => {
+//         const {
+//             data: {
+//                 data: {htmlRefer},
+//             },
+//         } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
+//         this.setState({refers: htmlRefer, isLoading: false})
+//         this.mainAnimation();
+//     }
+//     componentDidMount(){
+//         setTimeout(() => {
+//             document.getElementById("loading").classList.remove("loading__active");
+//             document.querySelector("body").style.background = "#F0EEEB";
+//             this.getSite();
+//         }, 2000);
+//     }
+//     render(){
+//         const {isLoading, refers} = this.state;
+//         return (
+//             <>
+//                 {isLoading ? (
+//                     <Loading color="light" />
+//                 ) : (
+//                     <>
+//                         <Header color="light" />
+//                         <Contents>
+//                             <ContTitle title={["Reference","book"]} color="light" />
+//                             <ReferCont refers={refers} color="light" />
+//                             <ContContact />
+//                         </Contents>
+//                         <Footer color="light" />
+//                     </>
+//                 )}
+//             </>
+//         )
+//     }
 // }
+// export default Reference;
+
+
 class Reference extends React.Component {
     state = {
-        isLoading: true,
-        refers: [],
+        isLoading : true,
+        refers :[],
     }
+
     mainAnimation = () => {
         setTimeout(() => {
             gsap.to("#header", {
-                duration: 0.8,
-                top: 0,
-            });
-            gsap.to("#footer", {
-                duration: 0.8,
-                bottom: 0,
-                delay: 0.2,
-            });
-            gsap.to(".cont__title strong", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.0,
-                ease: "power4.out",
-                scale : 1,
-            });
-            gsap.to(".cont__title em", {
-                duration: 0.7,
-                y: 0,
-                opacity: 0.9,
-                delay: 1.3,
-                ease: "power4.out",
-                scale : 1,
-            });
-            gsap.to(".refer__inner", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.5,
-                ease: "power4.out"
-            });
-        }, 1000)
+                                 duration: 0.8,
+                                 top: 0,
+                             });
+                             gsap.to("#footer", {
+                                 duration: 0.8,
+                                 bottom: 0,
+                                 delay: 0.2,
+                             });
+                             gsap.to(".cont__title strong", {
+                                 duration: 0.7,
+                                 y: 0,
+                                 opacity: 1,
+                                 delay: 1.0,
+                                 ease: "power4.out",
+                                 scale : 1,
+                             });
+                             gsap.to(".cont__title em", {
+                                 duration: 0.7,
+                                 y: 0,
+                                 opacity: 0.9,
+                                 delay: 1.3,
+                                 ease: "power4.out",
+                                 scale : 1,
+                             });
+                             gsap.to(".refer__inner", {
+                                 duration: 0.7,
+                                 y: 0,
+                                 opacity: 1,
+                                 delay: 1.5,
+                                 ease: "power4.out"
+                             });
+        }, 10)
     }
-    getSite = async () => {
+
+    getRefers = async () => {
         const {
-            data: {
-                data: {htmlRefer},
-            },
-        } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
-        this.setState({refers: htmlRefer, isLoading: false})
+            data : {
+                data : {htmlRefer}
+            }
+        } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json")
+
+        this.setState({refers : htmlRefer, isLoading : false,})
         this.mainAnimation();
     }
+
     componentDidMount(){
         setTimeout(() => {
             document.getElementById("loading").classList.remove("loading__active");
-            document.querySelector("body").style.background = "#F0EEEB";
-            this.getSite();
-        }, 2000);
+            this.getRefers();
+        },2000)
     }
+
     render(){
         const {isLoading, refers} = this.state;
+
         return (
             <>
                 {isLoading ? (
@@ -89,8 +163,45 @@ class Reference extends React.Component {
                     <>
                         <Header color="light" />
                         <Contents>
-                            <ContTitle title={["Reference","book"]} color="light" />
-                            <ReferCont refers={refers} color="light" />
+                            <ContTitle title={["Reference","book"]} />
+                            <section className="refer__cont">
+                                <div className="container">
+                                    <div className="refer__inner">
+                                        <h2>CSS</h2>
+                                        <ul className="refer__list">
+                                            {refers.map((refer) => (
+                                                  <ReferCont 
+                                                        key = {refer.id}
+                                                        id = {refer.id}
+                                                        title = {refer.title}
+                                                        desc = {refer.desc}
+                                                        use = {refer.use}
+
+                                                        desc2 = {refer.desc2}
+                                                        element = {refer.element}
+                                                        tag = {refer.tag}
+                                                        version = {refer.version}
+                                                        view = {refer.view}
+                                                        image = {refer.image}
+                                                        link = {refer.link}
+                                                        Definition = {refer.Definition}
+                                                        Accessibility = {refer.Accessibility}
+                                                        Related = {refer.Related}
+                                                        mdn = {refer.mdn}
+                                                        w3c = {refer.w3c}
+                                                  />
+                                            ))}
+                                                                            
+                                            {/* <li>
+                                                <a href="/">
+                                                    <span></span>
+                                                </a>
+                                            </li> */}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </section>
+                            {/* <ReferCont refers={refers} color="light" /> */}
                             <ContContact />
                         </Contents>
                         <Footer color="light" />
@@ -100,4 +211,5 @@ class Reference extends React.Component {
         )
     }
 }
-export default Reference;
+
+export default Reference
--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -5,19 +5,111 @@ import Footer from "../layout/Footer";
 import ScriptCont from "../includes/ScriptCont";
 import Contact from "../layout/Contact";
 import ContTitle from "../includes/ContTitle";
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
 
-function Youtube(){
-    return (
-        <>
-            <Header />
-            <Contents>
-                <ContTitle title={["Youtube", "book"]}/>
-                <ScriptCont />
-                <Contact />
-            </Contents>
-            <Footer />
-        </>
-    )
+
+
+
+// function Youtube(){
+//     return (
+//         <>
+//             <Header />
+//             <Contents>
+//                 <ContTitle title={["Youtube", "book"]}/>
+//                 <ScriptCont />
+//                 <Contact />
+//             </Contents>
+//             <Footer />
+//         </>
+//     )
+// }
+
+
+
+
+class Youtube extends React.Component {
+    state = {
+        isLoading: true,
+        ports: [],
+    }
+
+    mainAnimation = () => {
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.4,
+                top: 1,
+            });
+            gsap.to("#footer", {
+                duration: 0.4,
+                bottom: 1,
+            });
+            gsap.to(".cont__title strong", {
+                duration: 0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.0,
+                scale:1,
+            })
+            gsap.to(".cont__title em", {
+                duration: 0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.0,
+                scale:1,
+            })
+          
+        },10)
+    }
+
+    getPorts = async () => {
+        // const {data : {data: {ports}}} = await axios.get("https://webstoryboy.github.io/dothome1/portfolio.json");
+
+        // this.setState({ports: ports})
+        // console.log(ports)
+
+
+        setTimeout(() => {
+            console.log("두번째 시작")
+            this.setState({isLoading: false});
+            this.mainAnimation()
+        },1600)
+    }
+
+
+    componentDidMount(){
+        setTimeout(() => {
+            console.log("첫번째 시작")
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background = "#000";
+            this.getPorts();
+        }, 2000)
+    }
+
+    render(){
+        const {isLoading, ports} = this.state
+
+        console.log(ports)
+        return (
+            <>
+                {isLoading ? (
+                    <Loading />
+                ) : (
+                    <>
+                        <Header />
+                        <Contents>
+                            <ContTitle title={["Youtube", "book"]}/>
+                            <ScriptCont />
+                            <Contact />
+                        </Contents>
+                        <Footer />
+                    </>
+                )}
+            </>    
+        )
+    }
 }
 
 export default Youtube;
