0da512147390b66273153413b9688b6b52eb7911
boseungdl <96tmdqh@naver.com>
2022-04-21 10:45:33 +0900

    4

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,98 +1,103 @@
+
 import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import ContTitle from "../includes/ContTitle";
+import ContContact from "../includes/ContContact";
 import ReferCont from "../includes/ReferCont";
 import Loading from "../basics/Loading";
 import { gsap } from "gsap";
-
-class Reference extends React.Component {
-    state = {
-        isLoading: true,
-    }
-    getSite = () => {
-        setTimeout(() => {
-            gsap.to("#header", {
-                duration: 0.4,
-                top: 1,
-            });
-            gsap.to("#footer", {
-                duration: 0.4,
-                bottom: 1,
-            });
-            gsap.to(".cont__title strong", {
-                duration: 0.5,
-                x:0,
-                y:0,
-                opacity:1,
-                delay:1.0,
-                scale:1,
-            })
-            gsap.to(".cont__title em", {
-                duration: 0.5,
-                x:0,
-                y:0,
-                opacity:1,
-                delay:1.0,
-                scale:1,
-            })
-        }, 1000)
-    }
-
-    getRefer = () => {
-        setTimeout(() => {
-            console.log("두번째 시작")
-            this.setState({isLoading: false});
-            this.getSite()
-        },1600)
-    }
-
-  
-
-    componentDidMount(){
-        setTimeout(() => {
-            // this.setState({isLoading:false})
-            this.getRefer();
-        }, 1000)
-    }
-
-    render(){
-        const {isLoading} = this.state;
-
-     
-
-        return (
-            <>
-                {isLoading ? (
-                    <Loading />
-                ) : (
-                    <>
-                        <Header />
-                        <Contents>
-                            <ContTitle title = {["REFERENCE", "book"]}/>
-                            <ReferCont />
-                        </Contents>
-                        <Footer />
-                    </>
-                )}
-            </>    
-        )
-    }
-}
-
-
+import axios from "axios";
 // function Reference(){
 //     return (
 //         <>
-//             <Header />
+//             <Header color="light" />
 //             <Contents>
-//                 <ContTitle title = {["REFERENCE", "book"]}/>
-//                 <ReferCont color="light"/>
+//                 <Title title={["reference","book"]} color="light" />
+//                 <ReferCont color="light" />
+//                 <Contact />
 //             </Contents>
-//             <Footer />
+//             <Footer color="light" />
 //         </>
 //     )
 // }
-
+class Reference extends React.Component {
+    state = {
+        isLoading: true,
+        refers: [],
+    }
+    mainAnimation = () => {
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.8,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.2,
+            });
+            gsap.to(".cont__title strong", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.0,
+                ease: "power4.out",
+                scale : 1,
+            });
+            gsap.to(".cont__title em", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.3,
+                ease: "power4.out",
+                scale : 1,
+            });
+            gsap.to(".refer__inner", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.5,
+                ease: "power4.out"
+            });
+        }, 10)
+    }
+    getSite = async () => {
+        const {
+            data: {
+                data: {htmlRefer},
+            },
+        } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
+        this.setState({refers: htmlRefer, isLoading: false})
+        this.mainAnimation();
+    }
+    componentDidMount(){
+        setTimeout(() => {
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background = "#F0EEEB";
+            this.getSite();
+        }, 2000);
+    }
+    render(){
+        const {isLoading, refers} = this.state;
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light" />
+                ) : (
+                    <>
+                        <Header color="light" />
+                        <Contents>
+                            <ContTitle title={["Reference","book"]} color="light" />
+                            <ReferCont refers={refers} color="light" />
+                            <ContContact />
+                        </Contents>
+                        <Footer color="light" />
+                    </>
+                )}
+            </>
+        )
+    }
+}
 export default Reference;
